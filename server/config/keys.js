a40c77f70f7d0cead6c8355778547451478f1bbb
kariants <31441523+kariants@users.noreply.github.com>
2018-11-06 15:51:48 +0200

    Initial commit

---
+++ server/config/keys.js
@@ -0,0 +1,9 @@
+//keys
+
+if (process.env.NODE_ENV === 'production') {
+  //return production keys
+  module.exports = require('./prod');
+} else {
+  //return development keys
+  module.exports = require('./dev');
+}
